4929d8016fd1245beb76a3724ba3657a02b3a6ca
gka6428 <162865825+gka6428@users.noreply.github.com>
2024-11-25 23:09:12 +0900

    2024-11-15: I have begun to the implementation of the input unit of data. Here's the source code.

--- input_data.h
+++ input_data.h
@@ -1,56 +1,163 @@
-// 데이터 입력
-#ifndef _INPUT_DATA_H // 이 헤더 파일을 불러오는데 정의가 안되어 있으면,
-#define _INPUT_DATA_H // 아래 내용을 복붙해서 정의하시오.
+// 데이터 입력에 대한 헤더 파일입니다.
+/* 수질관리시스템의 기능 중 해당 파일은 아래와 같은 목적을 가집니다.
+
+*/
+#ifndef INPUT_DATA_H
+#define INPUT_DATA_H
 
 # include <stdio.h>
-#define YEAR_SIZE 100               //
-#define ROUND_SIZE 15               // 9 + 2 * 3 byte
-#define INDICATORS_SIZE 9           //
-#define THE_MEASURE_OF_REFERENCES 1 //
-#define THE_NUMBER_OF_REGION 10     // 수계 10곳
-#define NAME_SIZE 15                // 받을 지역명의 길이에 따라 조정해야함.
+# include <string.h>
+#define ARRAY_SIZE_OF_NAME_OF_REGION 30      // 측정지역명을 담을 배열의 길이 지정               
+#define ARRAY_SIZE_OF_YEAR 20                // 측정년도를 담을 배열의 길이 지정  
+#define ARRAY_SIZE_OF_THE_NUMBER_OF_ROUND 20 // 측정시기회차를 담을 배열의 길이 지정
+#define THE_NUMBER_OF_INDICATORS 9           // 저장할 지표의 개수 지정
+#define THE_NUMBER_OF_REGION 10              // 측정지역의 개수
+#define THE_NUMBER_OF_MEASURE 3              // 지표 단위의 개수(pH, mg/L, mL)
+
+// 사용자가 입력한 측정지역, 측정연도, 측정회차, 지표 정보 데이터에 대한 구조체 선언
+typedef struct indicators {
+    char name_of_region[ARRAY_SIZE_OF_NAME_OF_REGION];
+    char year[ARRAY_SIZE_OF_YEAR];
+    char round[ARRAY_SIZE_OF_THE_NUMBER_OF_ROUND];
+    char indicators[THE_NUMBER_OF_INDICATORS][2][50]; 
+} INDICATORS;
+
+// 초기화하지 않은 지표구조체 정의
+INDICATORS indi[THE_NUMBER_OF_REGION];
+
+char nameOfRegion_list[THE_NUMBER_OF_REGION][25] = {
+    // 각 측정지역의 영문명은 다음과 같다.
+    // 0)가양
+    "Gayang",
+    // 1)노량진
+    "Noryangjin",
+    // 2)도림천
+    "Dorimcheon",
+    // 3)안양천10
+    "Anyangcheon 10",
+    // 4)안양천9
+    "Anyangcheon 9",
+    // 5)중랑천5
+    "Jungnangcheon 5",
+    // 6)중랑천6
+    "Jungnangcheon 6",
+    // 7)청계천3
+    "Cheonggyecheon 3",
+    // 8)홍제천
+    "Hongjecheon",
+    // 9)보광
+    "Bogwang" 
+};
+
+const char indicators_list[THE_NUMBER_OF_INDICATORS][25] = {
+    // THE_NUMBER_OF_INDICATORS만큼 {}추가
+    "pH",           // pH
+    "BOD",          // mg/L
+    "COD",          // mg/L
+    "TOC",          // mg/L
+    "TSS",          // mg/L
+    "DO",           // mg/L
+    "T-P",          // mg/L
+    "총대장균군",    // mL
+    "분원성 대장균군"// mL
+};
+
+const char measure_list[THE_NUMBER_OF_MEASURE][10] = {
+    "pH",
+    "mg/L",
+    "mL"
+};
+
+
+void insert_info_to_struct(void);
+void insert_name_of_region_to_nameArr(INDICATORS* indicators, const int i);
+void insert_year_infor_to_yearArr(INDICATORS* indicators, char* yearArr);
+void insert_round_infor_to_roundArr(INDICATORS* indicators, char* roundArr);
+void insert_indicators_infor_to_indiArr(INDICATORS* indicators);
+void insert_measure_of_references_to_measureArr(INDICATORS* indicators);
+
+// 정의된 지표구조체에 정보 넣어주기
+void insert_info_to_struct(void)
+{
+    int i;
+    memset(&indi, 0, sizeof(indi));
+    // 절차 1) 각각의 지표구조체에 측정지역명 넣어주기(자동)
+    printf("지역명은 자동 입력됩니다.\n");
+    printf("입력 중입니다... 잠시만 기다려 주십시오.\n");
+    for (i = 0; i < THE_NUMBER_OF_REGION; i++)
+    {
+        insert_name_of_region_to_nameArr(&indi[i], i);
+    }
+    printf("입력 완료. \n\n");
+    
+    // 절차 2) 각각의 지표구조체에 측정연도, 측정회차 입력(수동)
+    for (i = 0; i < THE_NUMBER_OF_REGION; i++)
+    {
+        char yearArr[ARRAY_SIZE_OF_YEAR];
+        char roundArr[ARRAY_SIZE_OF_THE_NUMBER_OF_ROUND];
+
+        // 입력과 입력값을 구조체에 저장
+        printf("지역 (%d)의 이번 측정년도와 회차를 (xxxx년 x회차)의 형식으로 입력하시오. :", i);
+        scanf("%s %s", yearArr, roundArr);
+        insert_year_infor_to_yearArr(&indi[i], yearArr);
+        insert_round_infor_to_roundArr(&indi[i], roundArr);
+        printf("\n");
+    }
+    printf("입력 완료. \n\n");
+
+    // 절차 3) (미완)각각의 지표 구조체에 지표 입력(수동, 각 지표마다 단위 입력은 자동)
+    // for (i = 0; i < THE_NUMBER_OF_REGION; i++)
+    // {
+    //     insert_indicators_infor_to_indiArr(&indi[i]);
+    //     insert_measure_of_references_to_measureArr(&indi[i]);
+    // }
+    
+}
+
+// "정의된 지표구조체에 정보 넣어주기" 함수의 하위 함수들 
+void insert_name_of_region_to_nameArr(INDICATORS* indicators, const int i)
+{
+    strcpy(indicators->name_of_region, nameOfRegion_list[i]);
+    printf("%s\n", nameOfRegion_list[i]);
+}
+
+void insert_year_infor_to_yearArr(INDICATORS* indicators, char* yearArr)
+{   
+    char *ptr = strtok(yearArr,"년");
+    strcpy(indicators->year, ptr);
+    printf("%s가 연도에 저장됨.\n", indicators->year);
+}
+
+void insert_round_infor_to_roundArr(INDICATORS* indicators, char* roundArr)
+{   
+    char *ptr = strtok(roundArr,"회");
+    strcpy(indicators->round, ptr);
+    printf("%s가 측정회차에 저장됨.\n", indicators->round);
+}
+
+// void insert_indicators_infor_to_indiArr();
+
+// 지역 리스트 변경(삭제 후 추가 기능/추가 기능)
+
+#endif // INPUT_DATA_H
 
 // 사용자가 입력한 데이터를 받아올 구조체 정의
-typedef struct indicators {
-    char name_of_region[NAME_SIZE]; // 지역(수계)명
-	short year[YEAR_SIZE];          // 측정연월, int 대신 short로
-	char round[ROUND_SIZE];         // 회차, 10회차 안넘어가니 char로 선언
-	float indicators[INDICATORS_SIZE][THE_MEASURE_OF_REFERENCES]; // 지표[지표개수][각지표의단위]
-    // **주의) 수치를 매우 정확히 비교하여야 한다면 자료형을 다시 고려하자
-} INDICATORS;
-INDICATORS indicators_list[THE_NUMBER_OF_REGION]; // 지표구조체[측정지역수] 일단 선언
+    // 구조체
+        // 측정지역명에 대한 배열 선언 
+        // 측정년도에 대한 배열 선언   
+        // 측정회차에 대한 배열 선언
+        // 지표들 선언 -> 지표[지표의 개수][해당 지표에 담을 정보의 개수][문자열 입력을 위해 확보할 배열의 길이]
 
-// 각각의 지표구조체에 수계명 넣어주기
-void insert_name_of_region_to_nameList(INDICATORS* indicators_lst);
-/*
-{
-    // 각 측정지역의 영문명은 다음과 같다.
-    // 가양 - Gayang
-    // 노량진 - Noryangjin
-    // 도림천 - Dorimcheon
-    // 안양천10 - Anyangcheon 10
-    // 안양천9 - Anyangcheon 9
-    // 중랑천5 - Jungnangcheon 5
-    // 중랑천6 - Jungnangcheon 6
-    // 청계천3 - Cheonggyecheon 3
-    // 홍제천 - Hongjecheon
-    // 보광 - Bogwang
-}
-*/
+// 사용자의 데이터 입력 (1: 일일히 입력하는 경우) (오류가 있을 경우는 아직 고려안함)
+    // 지표구조체의 포인터를 매개변수로 받는다.
+    // 아래의 과정을 반복한다
+        // 
 
-// 지표구조체 내 각각의 지표들에 대해 단위 삽입
-void insert_measure_of_references_to_indicators(INDICATORS* indicators_lst);
-/*
-{
-    pH단위일 경우 pH = 0
-    mg/L단위일 경우 mgL = 1
-    mL단위일경우 mL = 2
-}
-*/
-
-// 엑셀 지표정보 받아오기
-void load_data_to_indicators();
-// 2) 입력을 어떻게 받을까?
-
-
-#endif // _INPUT_DATA_H
+// 각각의 지표구조체에 정보 넣어주기
+    // 각각의 지표구조체에 측정지역명 순서대로 넣어주기
+        //(미정) load_on_data.h애서 정의된 구조체를 가져와 정보를 그대로 복사하여 옮기기
+    // 각각의 지표구조체에 측정년도 순서대로 넣어주기
+    // 각각의 지표구조체에 측정회차 순서대로 넣어주기
+    // 각각의 지표구조체에 지표들 순서대로 넣어주기
+    // 지표구조체 내 각각의 지표들에 대해 단위 삽입
+    
